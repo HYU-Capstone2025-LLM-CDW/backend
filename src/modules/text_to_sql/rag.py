a3ac047ba5863b8504b8761f4e60168df738fcdc
namyun1012 <96728184+namyun1012@users.noreply.github.com>
2025-03-31 15:48:01 +0900

    [fix] rag, requirements

--- src/modules/text_to_sql/rag.py
+++ src/modules/text_to_sql/rag.py
@@ -3,22 +3,22 @@ import numpy as np
 from sentence_transformers import SentenceTransformer
 
 
-# 임베딩 모델 로드, 한글을 더 잘 지원해주는 임베딩 모델을 사용해야 할 필요성이 있다.
-embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
+# 임베딩 모델 로드, 영어 지원원
+_embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
 
-# search_history 에서 불러오는 작업 필요
-QUERY_LIST = ["Make SQL person group by race"]
-SQL_LIST = ["SELECT race_concept_id, COUNT(*) AS person_count FROM person GROUP BY race_concept_id"]
+# 추후 search_history(LOG), data set 등 에서 불러오는 작업 필요
+_query_list = ["Make SQL person group by race"]
+_sql_list = ["SELECT race_concept_id, COUNT(*) AS person_count FROM person GROUP BY race_concept_id"]
 
 
 # Vector DB 생성
-query_index = faiss.IndexFlatL2(embedding_model.get_sentence_embedding_dimension())
-query_vectors = embedding_model.encode(QUERY_LIST)
-query_index.add(np.array(query_vectors))
+_query_index = faiss.IndexFlatL2(_embedding_model.get_sentence_embedding_dimension())
+_query_vectors = _embedding_model.encode(_query_list)
+_query_index.add(np.array(_query_vectors))
 
-# Query 와 유사했던 이전의 Query 를 찾고 그에 대한 SQL을 Example 로 보내는 함수
+# Query 와 유사했던 이전의 Query 를 찾고 그에 대한 SQL을 Example 로 보내는 함수, 현재는 3개의 example 선정 중
 def retrieve_relevant_query(query: str, top_k: int = 3) -> list:
-    query_vector = embedding_model.encode([query])
-    distances, indices = query_index.search(query_vector, top_k)
+    query_vector = _embedding_model.encode([query])
+    distances, indices = _query_index.search(query_vector, top_k)
     
-    return [SQL_LIST[i] for i in indices[0]]
+    return [_sql_list[i] for i in indices[0]]
