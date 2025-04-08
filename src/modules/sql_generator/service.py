c276ed30b3f4a54a6f1665c16482d384631f0fd5
namyun1012 <96728184+namyun1012@users.noreply.github.com>
2025-04-08 18:21:49 +0900

    [fix] empty exception

--- src/modules/sql_generator/service.py
+++ src/modules/sql_generator/service.py
@@ -14,12 +14,17 @@ def _get_prompt() -> str:
     
 def generate(sqlGeneratorRequestDto: SqlGeneratorRequestDto) -> SqlGeneratorResponseDto:
     model_service = gemini_service
+    prompt = _get_prompt()
     
     #RAG를 사용한 Example 을 반영하는 코드
-    prompt = _get_prompt()
-    prompt += "\n 아래 몇 개의 예시가 있습니다. \n"
-    prompt += "\n".join(_add_relevant_query(sqlGeneratorRequestDto.text))
+    example = _add_relevant_query(sqlGeneratorRequestDto.text)
     
+    # Example 이 존재할 때만 예시 추가
+    if example:
+        prompt += "\n There are some few examples \n"
+        prompt += "\n".join(example)
+        prompt += "\n Please answer the questions based on the reference documents above."
+
     result = model_service.generate_response(prompt, sqlGeneratorRequestDto)
     content = result.content
     
@@ -28,7 +33,7 @@ def generate(sqlGeneratorRequestDto: SqlGeneratorRequestDto) -> SqlGeneratorResp
         error=content.get("error")
     )
 
-""" RAG """ 
+""" RAG(Retrieval-Augmented Generation) """ 
 
 # 임베딩 모델 로드, 영어 지원
 _embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
@@ -37,14 +42,20 @@ _embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
 _query_list = ["Make SQL person group by race"]
 _sql_list = ["SELECT race_concept_id, COUNT(*) AS person_count FROM person GROUP BY race_concept_id"]
 
-# Vector DB 생성, 현재는 서버를 실행할 때 생성함
+# Vector DB(_query_index) 생성, 현재는 서버를 실행할 때 생성함
 _query_index = faiss.IndexFlatL2(_embedding_model.get_sentence_embedding_dimension())
+
+# _query_list 를 vector 화 시켜서  vector DB 에 추가
 _query_index.add(np.array(_embedding_model.encode(_query_list)))
 
 # Query 와 유사했던 이전의 Query 와 그에 대한 SQL을 Example 로 보내는 함수, top_k개의 example 선정
 # sql_generator 의 service.py 에서만 실행하므로 private 함수로 설정
 def _add_relevant_query(query: str, top_k: int = 1) -> list[str]:
+    
+    # 사용자의 Query 를 Vector 화 후 Vector DB 에서 비슷하다고 판단되는 Query의 index 를 찾고 반환
     query_vector = _embedding_model.encode([query])
     distances, indices = _query_index.search(query_vector, top_k)
     
+    # 현재는 유사하다고 판단되는 example 을 top_k 만큼 보내는 형식
+    # 유사한 정도인 distance 는 사용하지 않고 있으나 추후 최소치 같은 방법으로 사용할 수도 있음.
     return [(f"query : {_query_list[i]}, sql : {_sql_list[i]}") for i in indices[0]]
