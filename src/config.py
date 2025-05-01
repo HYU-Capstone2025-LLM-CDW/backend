9662fa5a89f5eb0ef7aa229a8079ad90c6edd53c
joominjae <raipier8818@gmail.com>
2025-05-01 16:30:28 +0900

    feat: apply alembic

--- src/config.py
+++ src/config.py
@@ -5,6 +5,14 @@ class _Settings(BaseSettings):
     gemini_api_key: str
     cors_origins: list[str] = ["*"]
     
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+    
+    database_url: str = ""
+    db_host: str
+    db_port: int
+    
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
