a0b38920d98763a4022f7a16c109c06230a07698
joominjae <raipier8818@gmail.com>
2025-05-01 19:45:47 +0900

    fix: build

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
 class _Settings(BaseSettings):
-    gemini_api_key: str
+    gemini_api_key: str = ""
     cors_origins: list[str] = ["*"]
     
     postgres_user: str
