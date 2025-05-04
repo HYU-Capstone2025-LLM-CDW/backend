7fa61c9fdb7b7226a96b10e61f06dda6feb3bf3a
joominjae <raipier8818@gmail.com>
2025-05-04 19:50:13 +0900

    fix: bugs

--- src/modules/sql_executor/router.py
+++ src/modules/sql_executor/router.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from src.database import get_db
 from src.modules.sql_executor import service as sql_executor_service
 from src.modules.sql_executor.dto import SqlExecutorRequestDto, SqlExecutorResponseDto
 
@@ -8,5 +9,8 @@ router = APIRouter(prefix="/sql-executor", tags=["Text to SQL"])
 
 
 @router.post("/")
-async def sql_executor(sqlExecutorRequestDto: SqlExecutorRequestDto) -> SqlExecutorResponseDto:
-    return sql_executor_service.execute(sqlExecutorRequestDto)
+async def sql_executor(
+    sqlExecutorRequestDto: SqlExecutorRequestDto,
+    db: Session = Depends(get_db)
+) -> SqlExecutorResponseDto:
+    return await sql_executor_service.execute(sqlExecutorRequestDto, db)
