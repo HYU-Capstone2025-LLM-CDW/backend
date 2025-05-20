0a7bdf6010d80cc7472a4ab987a245e849647de1
sua01 <sua0211@naver.com>
2025-05-20 19:51:46 +0900

    [chore] modify dto.py

--- src/modules/sql_executor/dto.py
+++ src/modules/sql_executor/dto.py
@@ -3,8 +3,6 @@ from fastapi import HTTPException
 from pydantic import BaseModel, Field, field_validator
 
 from src.validator.sql_validator.basic_sql_validator import BasicSQLValidator   # 기본 검증
-from src.validator.sql_validator.safety_sql_validator import SQLComplexityLimiter  # 쿼리 복잡도 제한
-from src.validator.sql_validator.secure_sql_validator import SQLSecurityValidator # 민감정보 보호
 from src.validator.sql_validator.syntax_sql_validator import SQLSyntaxStructureValidator # 문법 및 구조 검사
 
 
@@ -16,10 +14,6 @@ class SqlExecutorRequestDto(BaseModel):
         try:
             # 쿼리 기본 검증
             BasicSQLValidator(value).validate()
-            # 쿼리 복잡도 검사
-            SQLComplexityLimiter(value).validate()
-            # 쿼리 민감 정보 보호
-            SQLSecurityValidator(value).validate()
             # 쿼리 문법 및 구조 검사
             SQLSyntaxStructureValidator(value).validate()
         except Exception as e:
